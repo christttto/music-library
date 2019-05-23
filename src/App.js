4f61399d307b5e0efc35843730a958711a966d75
Kris <christttto@berkeley.edu>
2019-05-23 03:26:18 -0700

    set state for all, tracked

--- src/App.js
+++ src/App.js
@@ -6,19 +6,38 @@ import "./App.css"
 class App extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      tracksJson: {},
+      starred: {}
+    }
   }
   componentDidMount() {
-    console.log("h1")
+    //use fetch, promise
+    //fetch music here, put it into state
+    fetch(
+      "https://gist.githubusercontent.com/paul-tcell/3803920c0570eb5ce83309bdd5d07ce4/raw/174eac7dc721cf83e2cee98710be947b8f526f87/100songs.json"
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          tracksJson: data
+        })
+      })
   }
-
+  //needs handle change when star pushed
+  //json is added to our starred state
+  //which will re-render the component
   render() {
     return (
       <div className="App">
         <List />
-        {/*list of all */}
+        {/*list of all, needs parameter to receive the tracks
+        this.state.tracksJson.tracks.track
+         */}
         <List />
-        {/*list of starred */}
+        {/*list of starred, parameter set to starred 
+        the starred will have same format as track: Array[100]
+        */}
       </div>
     )
   }
