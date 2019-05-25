c13450415536dda57278761d79f8a2304b304174
Kris <christttto@berkeley.edu>
2019-05-25 14:35:16 -0700

    log on click change

--- src/library/Song.js
+++ src/library/Song.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactStars from "react-stars"
-import Rating from "react-rating"
 import "../App.css"
 function Song(props) {
   const rateChange = newRate => {
@@ -9,21 +8,19 @@ function Song(props) {
   return (
     <div className="all-list-song">
       {/**onChange={} is to reflect change, value={} should be changed */}
-      <span>
-        <ReactStars count={1} size={24} color2={"teal"} half={false} />
-      </span>
-      <span>
-        <Rating
-          stop={1}
-          initialRating={0}
-          onChange={rateChange}
-          emptySymbol=""
-        />
-      </span>
-
+      <ReactStars
+        className="star"
+        count={1}
+        size={24}
+        color2={"teal"}
+        half={false}
+        onChange={newRate => {
+          rateChange(newRate)
+        }}
+      />
       <span className="track-name">{props.track.name} - </span>
       <span className="text">{props.track.artist.name}</span>
-      <div>{props.track.url}</div>
+      <div className="text">{props.track.url}</div>
     </div>
   )
 }
