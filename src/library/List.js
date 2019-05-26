18d2655940e9421bab0548268064456cf152a6e3
Kris <christttto@berkeley.edu>
2019-05-25 17:28:17 -0700

    isStar implemting

--- src/library/List.js
+++ src/library/List.js
@@ -1,21 +1,19 @@
-import React from "react"
+import React, { Component } from "react"
 import Song from "./Song"
 import "../App.css"
 
-function List(props) {
-  //needs to fetch json
-  //if json is empty, render empty
-  //if all, show empty star next to it
-  //if star, show yellow star
-
-  return (
-    <div>
-      list starts here
-      {props.tracksJson.map((t, index) => {
-        return <Song track={t} key={index} />
-      })}
-    </div>
-  )
+class List extends Component {
+  //<List tracksJson={tracks.track} star={this.star} />
+  render() {
+    return (
+      <div>
+        list starts here
+        {this.props.tracksJson.map((t, index) => {
+          return <Song track={t} key={index} star={this.props.star} />
+        })}
+      </div>
+    )
+  }
 }
 
 export default List
--- src/library/Song.js
+++ src/library/Song.js
@@ -2,9 +2,20 @@ import React from "react"
 import ReactStars from "react-stars"
 import "../App.css"
 function Song(props) {
+  let myValue = 0 //depends on the original value
   const rateChange = newRate => {
-    console.log(newRate)
+    myValue === 0 ? (myValue = 1) : (myValue = 0)
+    console.log(myValue)
+    return myValue
   }
+  const isStar = () => {
+    if (props.star.includes(props.track.url)) {
+      return 1
+    } else {
+      return 0
+    }
+  }
+
   return (
     <div className="all-list-song">
       {/**onChange={} is to reflect change, value={} should be changed */}
@@ -13,11 +24,13 @@ function Song(props) {
         count={1}
         size={24}
         color2={"teal"}
+        value={isStar(props.track.url)} //needs to be receiving from the parent component
         half={false}
         onChange={newRate => {
           rateChange(newRate)
         }}
       />
+
       <span className="track-name">{props.track.name} - </span>
       <span className="text">{props.track.artist.name}</span>
       <div className="text">{props.track.url}</div>
